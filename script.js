7318a933a20131fd0ce6f365c6ac6666dc1dd1bc
Akash Kurdekar <akashkurdekar39@gmail.com>
2023-08-18 14:37:46 +0530

    changes

--- script.js
+++ script.js
@@ -29,3 +29,39 @@ const linkAction = () =>{
     navMenu.classList.remove('show__menu') 
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+        const scrollDown = window.scrollY
+
+
+        sections.forEach(current =>{
+            const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+                if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight)
+                {
+                    sectionsClass.classList.add('active-link')
+                }
+                else
+                {
+                    sectionsClass.classList.remove('active-link')
+                }
+        })
+}
+window.addEventListener('scroll', scrollActive)
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+// When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll',scrollUp)
